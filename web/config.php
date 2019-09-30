3c32fbad317809681e58ab53576341166cc2c9f2
zyp997 <53375741+zyp997@users.noreply.github.com>
2019-10-01 03:50:14 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,10 +1,7 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', getenv("APIKEY")); //这里改成您的api key
-define('GJ_CODE', 'TW');//国家代码
-define('SITE_NAME', 'Youtube代理');//网站名字
-define('TITLENAME', 'Youtube代理');//网站名字
-define('EN2DEKEY', 'gfw-breaker');//一个随机字符串，加解密KEY
-define('EMAIL', 'lixiaoming0909@gmail.com');//邮箱
-define('PLAYLIST_ID', getenv("PLAYLIST_ID"));//推荐内容播放列表
+define('APIKEY', getenv("APIKEY")); 
+define('SITE_NAME', 'Youtube代理');
+define('TITLENAME', 'Youtube代理');
+
 ?>
