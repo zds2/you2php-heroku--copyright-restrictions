47a1e88e457279aa5283a652e56d6f82f3db26c2
zyp997 <53375741+zyp997@users.noreply.github.com>
2019-10-02 12:21:39 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -3,7 +3,7 @@ define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyBy2VkQXTgCWkfWfgejVUcKH5GuO2DFAds');
 define('GJ_CODE', 'JP');
 define('SITE_NAME', 'YouTube');
-define('TITLENAME', 'YouTube Mirror');
+define('TITLENAME', 'YouTube');
 define('EN2DEKEY', 'TXhEBihhDkmSpeHBBrrWahoPZUyS1jPsCwJGYgMoDClTpCTkbnJLQt7QHrhy5TjoFq4hvQ9VvkUoN1xoX1oyqIurmDdwwI2pJP');
 define('EMAIL', 'example@example.ne.jp');
 ?>
